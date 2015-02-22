375258651f22a35a43b0cd3d518077a8dbcc8e58
KenG98 <kengarber98@gmail.com>
2015-02-22 12:11:21 -0500

    some js
    WOOO

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 var decision_to_make = "";
 var factors = {};
+var options = {};
 
 function add_decision(){
 	decision_to_make = $("#decision-box").val();
 }
 
 function add_factor(){
-	var factorname = $().val();
+	var factorname = $("#factor-box").val();
+	$("#factor-box").val("");
+	var factorimportance = $("#factor-importance").val();
+	var wantHigh = $("#want-high").val();
+	factors[factorname] = {"importance":factorimportance, "want_high": wantHigh};
+}
+
+function add_option(){
+	var theOption = $("#option-box").val();
+	options[theOption] = {};
+}
+
+function test(){
+	console.log(factors);
 }
