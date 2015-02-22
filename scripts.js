57e75a9a1bf85b7bfe80e8cc90275111e632cd56
KenG98 <kengarber98@gmail.com>
2015-02-22 13:24:59 -0500

    more edits
    i dunno

--- scripts.js
+++ scripts.js
@@ -10,17 +10,29 @@ function add_factor(){
 	//add it to the json
 	var factorname = $("#factor-box").val();
 	$("#factor-box").val("");
-	var factorimportance = $("#factor-importance").val();
+	var factorimportance = parseInt($("#factor-importance").val());
 	var wantHigh = $("#want-high").val();
 	factors[factorname] = {"importance":factorimportance, "want_high": wantHigh};
 
 	//add it to the form
 	var nametemp = factorname.charAt(0).toUpperCase() + factorname.slice(1);
 	var formtemp = nametemp + "&nbsp; <input type='text' id='" + factorname + "'><br>"; //factorname &nbsp; <input type='text' id='factorname'>
-	$("#options").append(formtemp);
+	$("#add-after-this").after(formtemp);
 }
 
 function add_option(){
 	var theOption = $("#option-box").val();
-	options[theOption] = {};
+	var parameters = {}
+	for(factor in factors){
+		parameters[factor] = parseInt($("#" + factor).val());
+	}
+	options[theOption] = parameters;
+}
+
+function set_high_lows(){
+	for(factor in factors){
+		for(option in options){
+			
+		}
+	}
 }
