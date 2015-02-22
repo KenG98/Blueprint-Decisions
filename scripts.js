b4be90fffc2fcb8105672280a934de2d4c673338
KenG98 <kengarber98@gmail.com>
2015-02-22 12:47:04 -0500

    more changes
    JS CHANGES YEAH

--- scripts.js
+++ scripts.js
@@ -7,18 +7,20 @@ function add_decision(){
 }
 
 function add_factor(){
+	//add it to the json
 	var factorname = $("#factor-box").val();
 	$("#factor-box").val("");
 	var factorimportance = $("#factor-importance").val();
 	var wantHigh = $("#want-high").val();
 	factors[factorname] = {"importance":factorimportance, "want_high": wantHigh};
+
+	//add it to the form
+	var nametemp = factorname.charAt(0).toUpperCase() + factorname.slice(1);
+	var formtemp = nametemp + "&nbsp; <input type='text' id='" + factorname + "'><br>"; //factorname &nbsp; <input type='text' id='factorname'>
+	$("#options").append(formtemp);
 }
 
 function add_option(){
 	var theOption = $("#option-box").val();
 	options[theOption] = {};
-}
-
-function test(){
-	console.log(factors);
 }
