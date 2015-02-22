43cc8efd7d0503b6868f7ba7636d7ff856335eb8
KenG98 <kengarber98@gmail.com>
2015-02-22 11:43:25 -0500

    added some code
    javascripts

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 var decision_to_make = "";
+var factors = {};
+
+function add_decision(){
+	decision_to_make = $("#decision-box").val();
+}
+
+function add_factor(){
+	var factorname = $().val();
+}
